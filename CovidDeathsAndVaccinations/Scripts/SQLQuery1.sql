647049d24b30f5f57de2e39f97cdd20835a17496
davinbarron <davinbarron667@gmail.com>
2024-11-11 15:56:56 +0000

    Tableau tables and visualisation.

--- CovidDeathsAndVaccinations/Scripts/SQLQuery1.sql
+++ CovidDeathsAndVaccinations/Scripts/SQLQuery1.sql
@@ -157,3 +157,45 @@ Where dea.continent is not null
 
 Select *
 From PercentPopulationVaccinated
+
+----------------------------
+-- Creating Views for visualisation
+----------------------------
+
+
+-- 1. 
+
+Create view DeathPercentage as 
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2
+
+
+-- 2. 
+
+Create view TotalDeathCount as 
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+
+Create view PercentPopulationInfected as 
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+
+Create view PercentPopulationInfectedByDate as 
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
